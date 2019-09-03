aa98e07ad0873f067988b6009edfa292d933a53f
KEYtoSEE <43423808+keysee322@users.noreply.github.com>
2019-09-03 10:25:49 +0400

    Добавьте файлы проекта.

---
+++ Skobki/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Skobki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> stack = new Stack<char>();
+            string s = Console.ReadLine();
+            var chars = s.ToCharArray();
+            for (int i = 0; i< s.Length; i++)
+            {
+                if (chars[i] == '[' || chars[i] == '{' || chars[i] == '(')
+                    stack.Push(chars[i]);
+                else if (chars[i] == ']' && stack.Peek() == '[')
+                    stack.Pop();
+                else if (chars[i] == '}' && stack.Peek() == '{')
+                    stack.Pop();
+                else if (chars[i] == ')' && stack.Peek() == '(')
+                    stack.Pop();
+                else
+                    break;
+            }
+            if (stack.Count == 0)
+                Console.WriteLine("Последовательность правильная");
+            else Console.WriteLine("Последовательность неправильная");
+        }
+    }
+}
